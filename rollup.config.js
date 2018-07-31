0739afa1430f3edfd8aa9205d78ff6da00cd154a
Andy Likuski <andy@likuski.org>
2018-07-31 13:48:20 -0700

    Build fixes

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import replace from 'rollup-plugin-replace';
-import uglify from 'rollup-plugin-uglify';
+import { uglify } from 'rollup-plugin-uglify';
 import autoExternal from 'rollup-plugin-auto-external';
 
 const env = process.env.NODE_ENV;
